88c87408b1cc5681b97acaf0f863700a4d9385cf
rhmt <rezaeihazara@gmail.com>
2019-04-17 16:37:07 +0900

    config: install express js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const path = require('path')
 const port = process.env.PORT || 3000
 
-// app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/step-01.html');
+  res.sendFile(__dirname + '/src/index.html');
 });
-app.get('/main.js', (req, res) => {
-  res.sendFile(__dirname + '/main.js');
-});
+
+
+
+// app.get('/main.js', (req, res) => {
+//   res.sendFile(__dirname + '/main.js');
+// });
 
 app.listen(port);
