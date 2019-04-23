7b70a782c20c54d80d5fa262d0efd1c11fe4ce84
rhmt <rezaeihazara@gmail.com>
2019-04-23 21:32:19 +0900

    feat: get the use connection

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
-
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 app.use(express.static('src'))
+server.listen(port, () => {
+  console.log('listening on PORT', port)
+});
 
-app.listen(port);
+
+let users = [];
+
+const addUser = socket => {
+  users.push(socket.id)
+  console.log('users are: ', users);
+}
+
+const removeUser = socket => {
+  users = users.filter(user => user !== socket.id);
+  console.log(`socket id ${socket.id} is removed.`);
+};
+
+io.on('connection', socket => {
+  console.log('new user connected ...');
+  addUser(socket);
+  
+  socket.on('disconnect', () => {
+    removeUser(socket);
+  });
+})
+
